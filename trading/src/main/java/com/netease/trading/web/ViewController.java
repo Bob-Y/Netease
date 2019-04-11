8ec066ff5d0a13dc39008e1b74220427515b8cfc
zaixiandemiao <wangjialong093004@126.com>
2019-04-11 16:03:09 +0800

    增加viewController

--- trading/src/main/java/com/netease/trading/web/ViewController.java
+++ trading/src/main/java/com/netease/trading/web/ViewController.java
@@ -9,11 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class ViewController {
 
-    @RequestMapping("/html/*")
+    @RequestMapping("/html/**")
     public String login(HttpServletRequest request) {
         String requestURI = request.getRequestURI();
         System.out.println(requestURI);
-        String result = requestURI.substring(requestURI.lastIndexOf("/"));
+        int index = requestURI.lastIndexOf("html/");
+        String result = requestURI.substring(index);
         return result;
     }
 }
