8675108d2194d9fad47d779cdde4e5af5e0a7b7a
fengshilong <f_shilong@163.com>
2019-04-12 17:17:01 +0800

    fengshilong

---
+++ trading/src/main/webapp/resources/js/buyerProductDetail.js
@@ -0,0 +1,25 @@
+$(function () {
+    //从地址栏的url中获取商品id
+    var productId = getQueryString('id');
+    //获取商品信息的url
+    var userProductUrl = baseUrl+'/*****/' + productId;
+    $.getJSON(productUrl, function(data) {
+        if (data.success) {
+            var userProduct = data.product;
+            $('#product-img').attr('src',userProduct.productImgAddr);
+            $('#product-title').text(userProduct.productTitle);
+            $('#product-abstract').text(userProduct.productAbstract);
+            $('#product-price').text(userProduct.productPrice);
+            $('#product-detail').text(userProduct.productDetail);
+            var html = '';
+            if (userProduct.hasBought) {
+                html += '<button type="button" disabled="disabled">已购买</button>'
+            }else {
+                html += '<button type="button" id="add-to-cart">加入购物车</button>'
+            }
+            $('#user-button').html(html);
+        }
+    })
+
+
+})
---
+++ trading/src/main/webapp/resources/js/modify.js
@@ -0,0 +1,53 @@
+$(function () {
+    //从地址栏的url中获取商品id
+    var productId = getQueryString('id');
+    //获取商品信息的url
+    var productUrl = baseUrl+'/product/' + productId;
+    $.getJSON(productUrl, function(data) {
+        if (data.success) {
+            var product = data.product;
+            $('#product_title').val(product.productTitle);
+            $('#product_abstract').val(product.productAbstract);
+            $('#product_price').val(product.productPrice);
+            $('#product_detail').val(product.productPrice);
+        }
+    })
+
+    var modifyProductUrl = '/trading/productadmin/*****';
+    // 提交按钮的事件响应
+    $('#submit').click(function() {
+        // 创建product对象
+        var product = {};
+        // 获取表单里的数据并填充进对应的店铺属性中
+        product.productTitle = $('#product_title').val();
+        product.productAbstract = $('#product_abstract').val();
+        product.productDetail = $('#product_detail').val();
+        product.productPrice = $('#product_price').val();
+        product.productImgAddr = $('#product_image_address').val();
+        product.product_imgType = $('#product_imgType').val();
+        // if ($('#product_imgType').val().isEqual("1")) {
+        //     product.product_imgType = 1;
+        // } else {
+        //     product.product_imgType = 2;
+        // }
+        var formData = new FormData();
+        // 将shop json对象转成字符流保存至表单对象key为shopStr的的键值对里
+        formData.append('productStr', JSON.stringify(product));
+        // 将数据提交至后台处理相关操作
+        $.ajax({
+            url : modifyProductUrl,
+            type : 'POST',
+            data : formData,
+            contentType : false,
+            processData : false,
+            cache : false,
+            success : function(data) {
+                if (data.success) {
+                    alert('提交成功！');
+                } else {
+                    alert('提交失败！' + data.errMsg);
+                }
+            }
+        });
+    });
+})
