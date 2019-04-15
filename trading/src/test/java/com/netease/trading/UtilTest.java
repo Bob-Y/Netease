17da9fded74ca873df3c9099c0cceddb358f4af3
zaixiandemiao <wangjialong093004@126.com>
2019-04-15 15:46:55 +0800

    fix： 已购买

--- trading/src/test/java/com/netease/trading/UtilTest.java
+++ trading/src/test/java/com/netease/trading/UtilTest.java
@@ -3,6 +3,8 @@ package com.netease.trading;
 import com.netease.trading.util.MD5Util;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class UtilTest {
 
 
@@ -12,6 +14,11 @@ public class UtilTest {
         System.out.println("reyub " + reyub);
         String s1 = MD5Util.getMD5("123");
         System.out.println("s1 " + s1);
+    }
+
+    @Test
+    public void testDate() {
+        System.out.println(new Date(1555322390000L));
     }
 
 }
